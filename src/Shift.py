bf5715ccb69d9703ce6d86414daa04d1abd2e381
erikthehero <erikoudejans@gmail.com>
2022-07-17 20:14:00 +0200

    Added empty Shift class.

---
+++ src/Shift.py
@@ -0,0 +1,12 @@
+class Shift:
+    def __init__(self):
+        pass
+
+class Shifts:
+    def __init__(self):
+        self.shifts = []
+        pass
+
+    def initFromFile(fn):
+        assert(fn)
+        pass
